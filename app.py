5a0ca99af80bf8fd272e3c880e85834c75be9fa4
jeong chang min <ckdalsla1994@gmail.com>
2022-12-05 20:55:59 +0900

    get방식과 post방식을 웹개발종합반에서 배운 형태로 구현

--- app.py
+++ app.py
@@ -15,11 +15,27 @@ cursor = db.cursor()
 
 @app.route("/")
 def index():
+  return render_template('index.html')
+
+# GET 구현
+@app.route("/comment", methods=["GET"])
+def comment_get():
   sql = '''SELECT * FROM test_table'''
   cursor.execute(sql)
   data_list = cursor.fetchall()
-  return render_template('index.html', data_list = data_list)
+  return jsonify({'data_list': data_list})
 
+# POST 구현
+@app.route("/save_comment", methods=["POST"])
+def comment_post():
+  name_receive = request.form['name_give']
+  comment_receive = request.form['comment_give']
+  print(name_receive)
+  sql =f'''INSERT INTO test_table(name, comment, note)
+        VALUES('{name_receive}', '{comment_receive}', '비고');'''
+  cursor.execute(sql)
+  db.commit()
+  return jsonify({'msg':'댓글 입력 완료!'})
 
 
 if __name__ == '__main__':
