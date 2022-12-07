ec0da073e1d5491c06b5e5c8c3c1d1c97b7a4019
seungjun <lsjnl22@gmail.com>
2022-12-07 14:58:48 +0900

    로그인&회원가입 기능 완성 메인페이지 프론트 미완

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import pymysql
 import json
 # import hashlib
@@ -14,52 +14,78 @@ app = Flask(__name__)
 
 db = pymysql.connect(host='secendproj.cczokkdg0lti.ap-northeast-1.rds.amazonaws.com', port=3306, user='admin',
                      passwd='roqkfwkehlrhtlqwh', db='2_project', charset='utf8')
-
-
 cursor = db.cursor()
 
+app.secret_key = 'your secret key'
 
 @app.route('/')
-def login_signup_page():
-    return render_template('first_page.html')
+def login_page():
+    return render_template('login.html')
 
-@app.route('/index.html')
-def login_success():
+
+@app.route('/signup')
+def signup_page():
+    return render_template('signup.html')
+
+
+@app.route('/main')
+def main_page():
     return render_template('index.html')
+
+# 로그인 기능
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login_btn():
+    db = pymysql.connect(host='secendproj.cczokkdg0lti.ap-northeast-1.rds.amazonaws.com', port=3306, user='admin',
+                         passwd='roqkfwkehlrhtlqwh', db='2_project', charset='utf8')
+    cursor = db.cursor()
+
+    msg = ''
+
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+
+        username = request.form['username']
+        password = request.form['password']
+
+        cursor.execute(
+            'SELECT * FROM accounts WHERE username = %s AND pw = %s', (username, password))
+
+        account = cursor.fetchone()
+        print(account)
+
+        if account:
+            session['loggedin'] = True
+            session['id'] = account[0]
+            session['username'] = account[1]
+            msg = '로그인 성공'
+            return render_template('index.html', msg=msg)
+        else:
+            msg = '로그인 실패!'
+    return render_template('login.html', msg=msg)
+
+# 회원가입 기능
 
 
 @app.route("/signup", methods=["POST"])
 def signup_btn_click():
 
+    db = pymysql.connect(host='secendproj.cczokkdg0lti.ap-northeast-1.rds.amazonaws.com', port=3306, user='admin',
+                         passwd='roqkfwkehlrhtlqwh', db='2_project', charset='utf8')
+    cursor = db.cursor()
+
     name_receive = request.form['name_give']
+    nickname_receive = request.form['nickname_give']
     email_receive = request.form['email_give']
     password_receive = request.form['password_give']
 
-    sql = f'''INSERT INTO signup(name, email, pw)
-        VALUES('{name_receive}', '{email_receive}','{password_receive}');'''
+    sql = f'''INSERT INTO accounts(username, nickname, email, pw)
+        VALUES('{name_receive}','{nickname_receive}','{email_receive}','{password_receive}');'''
     cursor.execute(sql)
     db.commit()
     return jsonify({'msg': '회원가입 완료!'})
 
-# 로그인 기능 구현
 
-
-# @app.route('/login', methods=['POST'])
-# def login_btn_click():
-
-#     username = request.form['name_give']
-#     password = request.form['password_give']
-
-#     sql = f'''SELECT * FROM signup WHERE username = %s AND password = %s; '''
-#     account = cursor.fetchone()
-
-#     if account:
-#         session['loggedin'] = True
-#         session['id'] = account['id']
-#         session['username'] = account['username']
-#         return '로그인 성공!'
-#     else:
-#         msg = '로그인 실패!'
 
 
 if __name__ == '__main__':
